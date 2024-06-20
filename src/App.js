8c57d4c7029a4a8c70d203c86520c80fef156cb8
Nishita Pande <89330962+nishitapande@users.noreply.github.com>
2024-06-20 17:53:29 +0530

    added search feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Card from "./components/Card";
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     (async () => {
       const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
@@ -26,10 +27,26 @@ function App() {
       setData(detailedData);
     })();
   }, []);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredData = data.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="">
+    <div className="flex flex-col items-center">
+      <input
+        type="text"
+        placeholder="Search Pokémon..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className="overflow-hidden mt-6 w-11/12 md:w-1/2 p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
       <div className="flex flex-wrap gap-4 mx-4">
-        {data.map((item, index) => {
+        {filteredData.map((item, index) => {
           return <Card item={item} key={index} />;
         })}
       </div>
